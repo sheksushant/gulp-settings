2a209c3363ca2539d4148508213d2a72812f9c0a
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-07-09 18:40:21 +0530

    added nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 var jsFiles = ['*.js','src/**/*.js'];
 
 gulp.task('style', function() {
@@ -29,4 +30,19 @@ gulp.task('inject', function() {
       .pipe(inject(injectSrc,injectOptions))
       .pipe(gulp.dest('./src/views'));
 
+});
+
+gulp.task('serve',['style','inject'],function() {
+    var options =  {
+        script : 'app.js',
+        delayTime : 1,
+        env : {
+            'PORT' : 5000
+        },
+        watch : jsFiles
+    };
+    return nodemon(options)
+      .on('restart',function(ev) {
+        console.log('restarting');
+    });
 });
