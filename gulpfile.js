3fbfd5f86c32e22a035a663c9843e2750262c2e4
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-07-09 16:58:13 +0530

    upload guplfile

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var jsFiles = ['*.js','src/**/*.js'];
+
+gulp.task('style', function() {
+    gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', {
+        verbose : true
+    }))
+    .pipe(jscs());
+});
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read : false});
+    var injectOptions = {
+        ignorePath:'/public'
+    };
+    var options = {
+        bowerJson : require('./bower.json'),
+        directory  : './public/lib',
+        ignorePath : '../../public'
+    };
+    return gulp.src('./src/views/*.html')
+      .pipe(wiredep(options))
+      .pipe(inject(injectSrc,injectOptions))
+      .pipe(gulp.dest('./src/views'));
+
+});
